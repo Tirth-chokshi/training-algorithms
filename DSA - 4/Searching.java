ea608d3de983acf6c6d0b9ac42d52c22450f7bd0
Tirth chokshi <chokshitirth4@gmail.com>
2024-04-29 20:16:06 +0530

    binary search added

--- DSA - 4/Searching.java
+++ DSA - 4/Searching.java
@@ -4,7 +4,9 @@ public class Searching{
 	public static void main(String args[]){
 		int arr [] = {4,46,23,6,9,77};
         int target = 6;
-        System.out.println(linear(arr,target));
+        //System.out.println(linear(arr,target));
+		Arrays.sort(arr);
+		System.out.println(binary(arr,target));
 	}
 	
 	static int linear(int arr[] , int target){
@@ -16,5 +18,24 @@ public class Searching{
 		}
 		return-1;
 	}
-	
+	static int binary(int arr[],int target){
+		if(arr.length == 0){return-1;}
+		int start = 0;
+		int end = arr.length-1;
+		
+		while(start<= end){
+			int mid = start + (end - start) /2;
+			
+			if(target <arr[mid]){
+				end = mid-1;
+			}
+			else if (target > arr[mid]){
+				start = mid+1;
+			}
+			else{
+				return mid;
+			}
+		}
+		return-1;
+	}
 }
