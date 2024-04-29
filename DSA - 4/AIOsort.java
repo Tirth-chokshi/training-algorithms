bc6af294faa8bd9f4c927cb525b30bac0999d467
Tirth chokshi <chokshitirth4@gmail.com>
2024-04-29 19:16:41 +0530

    + quicksort

--- DSA - 4/AIOsort.java
+++ DSA - 4/AIOsort.java
@@ -9,7 +9,8 @@ public class AIOsort {
 //        bubble(num);
 //        selectionMin(num);
         //insertion(num2);
-		mergSort(num2);
+		//mergSort(num2);
+		//quicksort(num,0,num.length-1);
         System.out.println(Arrays.toString(num2));
     }
     static void insertionSort(int arr[])
@@ -81,6 +82,33 @@ public class AIOsort {
             }
         }
     }
+	
+	static void quicksort(int arr[] , int low , int high){
+		if(low >= high){
+			return;
+		}
+		int s = low;
+		int e = high;
+		int m = s+(e-s)/2;
+		int pivot = arr[m];
+		
+		while(s<=e){
+			while(arr[s] < pivot){
+				s++;
+			}
+			while(arr[e] > pivot){
+				e--;
+			}
+			if(s <=e ){
+				int temp = arr[s];
+				arr[s] = arr[e];
+				arr[e] = temp;
+			}
+		}
+		
+		quicksort(arr, low,e);
+		quicksort(arr, s, high);
+	}
 	
 	
 	static void mergSort(int arr[]){
